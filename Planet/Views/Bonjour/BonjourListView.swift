b89397d92d0e7d7eb5c3d270f1841b9092589531
Xin Liu <livid@v2ex.com>
2023-10-28 02:19:30 -0700

    Prefer IPv4 when resolving bonjour service

--- Planet/Views/Bonjour/BonjourListView.swift
+++ Planet/Views/Bonjour/BonjourListView.swift
@@ -65,14 +65,34 @@ extension BonjourViewModel: NetServiceBrowserDelegate {
 
 extension BonjourViewModel: NetServiceDelegate {
     func netServiceDidResolveAddress(_ sender: NetService) {
-        guard let addressData = sender.addresses?.first else { return }
+        guard let addressData = sender.addresses else { return }
+
+        // Filter IPv4 addresses and exclude 127.0.0.1
+        let filteredIPv4Addresses = addressData.filter { data in
+            let pointer = data.withUnsafeBytes { $0.bindMemory(to: sockaddr.self).baseAddress! }
+            return pointer.pointee.sa_family == AF_INET
+        }
+
+        let usableIPv4 = filteredIPv4Addresses.first { data in
+            var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+            let sockaddrPointer = data.withUnsafeBytes { $0.bindMemory(to: sockaddr.self).baseAddress! }
+            getnameinfo(sockaddrPointer, socklen_t(data.count), &hostname, socklen_t(hostname.count), nil, 0, NI_NUMERICHOST)
+            let ip = String(cString: hostname, encoding: .utf8)
+            return ip != "127.0.0.1"
+        }
+
+        // Use usable IPv4 if found, otherwise use the first address
+        let preferredAddress = usableIPv4 ?? addressData.first
+
+        guard let address = preferredAddress else { return }
 
         var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-        let sockaddrPointer = addressData.withUnsafeBytes { $0.bindMemory(to: sockaddr.self).baseAddress! }
+        let sockaddrPointer = address.withUnsafeBytes { $0.bindMemory(to: sockaddr.self).baseAddress! }
         
-        if getnameinfo(sockaddrPointer, socklen_t(addressData.count), &hostname, socklen_t(hostname.count), nil, 0, NI_NUMERICHOST) == 0 {
+        if getnameinfo(sockaddrPointer, socklen_t(address.count), &hostname, socklen_t(hostname.count), nil, 0, NI_NUMERICHOST) == 0 {
             if let ip = String(cString: hostname, encoding: .utf8) {
                 print("Service IP: \(ip), Port: \(sender.port)")
+
                 // Set this IP and port to settings view
                 let serverURL: String
                 if ip.contains(":") {
