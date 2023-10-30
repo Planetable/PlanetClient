f845468c6530267f648525f455cb7c174f9ec36b
Xin Liu <livid@v2ex.com>
2023-10-30 07:12:13 -0700

    Add title to BonjourListView

--- Planet/Views/Bonjour/BonjourListView.swift
+++ Planet/Views/Bonjour/BonjourListView.swift
@@ -5,25 +5,33 @@
 //  Created by Xin Liu on 10/28/23.
 //
 
-import SwiftUI
 import Foundation
+import SwiftUI
 
 struct BonjourListView: View {
     @ObservedObject var viewModel = BonjourViewModel()
 
     var body: some View {
-        List(viewModel.services, id: \.name) { service in
-            Button(action: {
-                viewModel.resolveService(service: service)
-            }) {
-                Text(service.name)
+        VStack(spacing: 0) {
+            HStack {
+                Text("Scanning nearby Planet servers")
+                Spacer()
+                ProgressView()
             }
-        }
-        .onAppear {
-            viewModel.startScanning()
-        }
-        .onDisappear {
-            viewModel.stopScanning()
+            .padding()
+            List(viewModel.services, id: \.name) { service in
+                Button(action: {
+                    viewModel.resolveService(service: service)
+                }) {
+                    Text(service.name)
+                }
+            }
+            .onAppear {
+                viewModel.startScanning()
+            }
+            .onDisappear {
+                viewModel.stopScanning()
+            }
         }
     }
 }
@@ -42,7 +50,7 @@ class BonjourViewModel: NSObject, ObservableObject {
         browser?.stop()
         browser = nil
     }
-    
+
     func resolveService(service: NetService) {
         service.delegate = self
         service.resolve(withTimeout: 5.0)
@@ -50,13 +58,21 @@ class BonjourViewModel: NSObject, ObservableObject {
 }
 
 extension BonjourViewModel: NetServiceBrowserDelegate {
-    func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
+    func netServiceBrowser(
+        _ browser: NetServiceBrowser,
+        didFind service: NetService,
+        moreComing: Bool
+    ) {
         DispatchQueue.main.async {
             self.services.append(service)
         }
     }
 
-    func netServiceBrowser(_ browser: NetServiceBrowser, didRemove service: NetService, moreComing: Bool) {
+    func netServiceBrowser(
+        _ browser: NetServiceBrowser,
+        didRemove service: NetService,
+        moreComing: Bool
+    ) {
         DispatchQueue.main.async {
             self.services.removeAll(where: { $0 == service })
         }
@@ -75,8 +91,18 @@ extension BonjourViewModel: NetServiceDelegate {
 
         let usableIPv4 = filteredIPv4Addresses.first { data in
             var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-            let sockaddrPointer = data.withUnsafeBytes { $0.bindMemory(to: sockaddr.self).baseAddress! }
-            getnameinfo(sockaddrPointer, socklen_t(data.count), &hostname, socklen_t(hostname.count), nil, 0, NI_NUMERICHOST)
+            let sockaddrPointer = data.withUnsafeBytes {
+                $0.bindMemory(to: sockaddr.self).baseAddress!
+            }
+            getnameinfo(
+                sockaddrPointer,
+                socklen_t(data.count),
+                &hostname,
+                socklen_t(hostname.count),
+                nil,
+                0,
+                NI_NUMERICHOST
+            )
             let ip = String(cString: hostname, encoding: .utf8)
             return ip != "127.0.0.1"
         }
@@ -87,9 +113,19 @@ extension BonjourViewModel: NetServiceDelegate {
         guard let address = preferredAddress else { return }
 
         var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-        let sockaddrPointer = address.withUnsafeBytes { $0.bindMemory(to: sockaddr.self).baseAddress! }
-        
-        if getnameinfo(sockaddrPointer, socklen_t(address.count), &hostname, socklen_t(hostname.count), nil, 0, NI_NUMERICHOST) == 0 {
+        let sockaddrPointer = address.withUnsafeBytes {
+            $0.bindMemory(to: sockaddr.self).baseAddress!
+        }
+
+        if getnameinfo(
+            sockaddrPointer,
+            socklen_t(address.count),
+            &hostname,
+            socklen_t(hostname.count),
+            nil,
+            0,
+            NI_NUMERICHOST
+        ) == 0 {
             if let ip = String(cString: hostname, encoding: .utf8) {
                 print("Service IP: \(ip), Port: \(sender.port)")
 
@@ -97,7 +133,8 @@ extension BonjourViewModel: NetServiceDelegate {
                 let serverURL: String
                 if ip.contains(":") {
                     serverURL = "http://[\(ip)]:\(sender.port)"
-                } else {
+                }
+                else {
                     serverURL = "http://\(ip):\(sender.port)"
                 }
                 PlanetSettingsViewModel.shared.serverURL = serverURL
@@ -111,8 +148,4 @@ struct BonjourListView_Previews: PreviewProvider {
     static var previews: some View {
         BonjourListView()
     }
-}
-
-#Preview {
-    BonjourListView()
 }
