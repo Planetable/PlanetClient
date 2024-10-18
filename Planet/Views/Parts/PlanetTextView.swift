3515c34e4e57f452678219da17e49c4e5e8e46ae
Kai <kai@v2ex.com>
2024-10-18 14:53:00 +0800

    Fixed a cursor position issue.

--- Planet/Views/Parts/PlanetTextView.swift
+++ Planet/Views/Parts/PlanetTextView.swift
@@ -24,12 +24,12 @@ struct PlanetTextView: UIViewRepresentable {
         }
 
         func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            let cursorPosition = textView.selectedRange.location
+            let content = textView.text as NSString
+            let currentLineRange = content.lineRange(for: NSRange(location: cursorPosition == 0 ? 0 : cursorPosition - 1, length: 0))
+            let currentLine = content.substring(with: currentLineRange)
             if text == "\n" {
                 // Add first-level auto completion for list items and checkboxs:
-                let cursorPosition = textView.selectedRange.location
-                let content = textView.text as NSString
-                let currentLineRange = content.lineRange(for: NSRange(location: cursorPosition == 0 ? 0 : cursorPosition - 1, length: 0))
-                let currentLine = content.substring(with: currentLineRange)
                 var symbol = ""
                 if currentLine.hasPrefix("* ") {
                     symbol = "* "
@@ -53,6 +53,9 @@ struct PlanetTextView: UIViewRepresentable {
                 }
                 if currentLine.hasSuffix("\n") {
                     // Should skip this symbol
+                    textView.text = content.replacingCharacters(in: range, with: "\n")
+                    textView.selectedRange = NSRange(location: cursorPosition + 1, length: 0)
+                    return false
                 } else if symbol == currentLine || symbol == currentLine + "\n" {
                     // Should remove previous auto completed list symbol if empty content returns
                     textView.text = content.replacingCharacters(in: currentLineRange, with: "\n")
@@ -71,6 +74,16 @@ struct PlanetTextView: UIViewRepresentable {
                         }
                         return false
                     }
+                }
+            } else if text.isEmpty {
+                // Check if the current line is empty after deletion
+                if currentLine.trimmingCharacters(in: .whitespacesAndNewlines).count == 1 {
+                    let previousLineRange = (content as NSString).lineRange(for: NSRange(location: max(currentLineRange.location - 1, 0), length: 0))
+                    let newCursorPosition = previousLineRange.location + previousLineRange.length - 1
+                    let newContent = content.replacingCharacters(in: currentLineRange, with: "")
+                    textView.text = newContent
+                    textView.selectedRange = NSRange(location: newCursorPosition, length: 0)
+                    return false
                 }
             }
             return true
